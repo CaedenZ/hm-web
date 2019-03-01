2c42c5bde684f2d9e13229b5abd5d12d077099a4
CaedenZ <FENGYE.16@ichat.sp.edu.sg>
2019-03-01 21:34:00 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './scenes/LoginPage';
+
+class App extends Component {
+  render() {
+    return (
+      <LoginPage/>
+    );
+  }
+}
+
+export default App;
