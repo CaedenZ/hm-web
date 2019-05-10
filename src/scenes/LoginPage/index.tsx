6b2e479eb50d22a3dcff1f249cdfb906e8de7b9d
Syoongy <mastersyong@gmail.com>
2019-05-11 03:42:00 +0800

    center and clean login page

--- src/scenes/LoginPage/index.tsx
+++ src/scenes/LoginPage/index.tsx
@@ -21,36 +21,41 @@ import { SharedDispatchProps } from "../../interface/propsInterface";
 import { RootState } from "../../reducer";
 
 const styles = (theme: any) => ({
+  centerLogin: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh"
+  },
   main: {
     width: "auto",
     display: "block", // Fix IE 11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto"
+      width: "40vh"
     }
   },
   paper: {
-    marginTop: theme.spacing.unit * 8,
     display: "flex",
-    // flexDirection: 'column',
     alignItems: "center",
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
-      .spacing.unit * 3}px`
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing.unit
-  },
+    justifyContent: "center",
+    padding: `${theme.spacing.unit * 3}px`
+  }
+  // avatar: {
+  //   margin: theme.spacing.unit,
+  //   backgroundColor: theme.palette.secondary.main
+  // },
+  // form: {
+  //   width: "100%", // Fix IE 11 issue.
+  //   marginTop: theme.spacing.unit
+  // }
 });
 
-export interface Props extends SharedDispatchProps, InState, WithStyles<typeof styles> { }
+export interface Props
+  extends SharedDispatchProps,
+    InState,
+    WithStyles<typeof styles> {}
 
 interface InState {
   usertoken: string;
@@ -61,15 +66,14 @@ export interface State {
   app_key: string;
 }
 
-
 class SignIn extends Component<Props, State> {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
-      app_key: 'p9Eg6HN7FFXjA9WTNZ5n'
-    }
+      email: "",
+      password: "",
+      app_key: "p9Eg6HN7FFXjA9WTNZ5n"
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.dynSetState = this.dynSetState.bind(this);
@@ -78,7 +82,7 @@ class SignIn extends Component<Props, State> {
   dynSetState(key: keyof State, value: string) {
     this.setState({
       [key]: value
-    } as Pick<State, keyof State>)
+    } as Pick<State, keyof State>);
   }
 
   handleChange(event) {
@@ -86,51 +90,67 @@ class SignIn extends Component<Props, State> {
     // console.dir(event.target)
   }
 
-  handleLogin = (event) => {
+  handleLogin = event => {
     event.preventDefault();
-    this.props.login(this.state)
+    this.props.login(this.state);
     // console.log('s')
-  }
+  };
 
   render() {
     const { classes } = this.props;
     if (this.props.usertoken === "") {
       return (
-        <main className={classes.main}>
-          <CssBaseline />
-          <Paper className={classes.paper}>
-            <form onSubmit={this.handleLogin}>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input type="email" id="email" name="email" autoComplete="email" autoFocus value={this.state.email} onChange={this.handleChange} />
-              </FormControl>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </FormControl>
-              {/* <FormControlLabel
+        <div className={classes.centerLogin}>
+          <main className={classes.main}>
+            <CssBaseline />
+            <Paper className={classes.paper}>
+              <form onSubmit={this.handleLogin}>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="email">Email Address</InputLabel>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  />
+                </FormControl>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
+                </FormControl>
+                {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
               /> */}
-              <Divider />
-              {/* <div style={{ marginTop: 20 }}>
+                <Divider />
+                {/* <div style={{ marginTop: 20 }}>
                 <Link to="/forgetpassword">Forget password</Link>
               </div> */}
-              <Button fullWidth variant="contained" color="primary" type="submit">Sign in</Button>
-            </form>
-          </Paper>
-        </main>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Sign in
+                </Button>
+              </form>
+            </Paper>
+          </main>
+        </div>
       );
-    }
-    else {
-      return (<Redirect to="/" />)
+    } else {
+      return <Redirect to="/" />;
     }
   }
 }
@@ -138,7 +158,10 @@ class SignIn extends Component<Props, State> {
 function mapStateToProps(state: any) {
   return {
     usertoken: state.authenticationReducer.token
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SignIn));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(SignIn));
