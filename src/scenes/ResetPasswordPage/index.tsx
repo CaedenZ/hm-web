4a8c7f0cc11aea5387822a4aa67f599264bb4bfe
Syoongy <mastersyong@gmail.com>
2019-05-11 20:04:44 +0800

    clean reset password page

--- src/scenes/ResetPasswordPage/index.tsx
+++ src/scenes/ResetPasswordPage/index.tsx
@@ -1,54 +1,53 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import { SharedDispatchProps } from '../../interface/propsInterface';
-import { mapDispatchToProps } from '../../helper/dispachProps';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles, { WithStyles } from "@material-ui/core/styles/withStyles";
+import { SharedDispatchProps } from "../../interface/propsInterface";
+import { mapDispatchToProps } from "../../helper/dispachProps";
+import { connect } from "react-redux";
 
 const styles = (theme: any) => ({
   main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
       width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
   },
   paper: {
     marginTop: theme.spacing.unit * 8,
-    display: 'flex',
+    display: "flex",
     // flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
   },
   avatar: {
     margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
   },
   submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
+    marginTop: theme.spacing.unit * 3
+  }
 });
 
-export interface Props extends SharedDispatchProps, InState, WithStyles<typeof styles> { }
+export interface Props
+  extends SharedDispatchProps,
+    InState,
+    WithStyles<typeof styles> {}
 
 interface InState {
   usertoken: string;
@@ -58,15 +57,13 @@ export interface State {
   confirmPassword: string;
 }
 
-
 class ResetPasswordPage extends Component<Props, State> {
-
   constructor(props) {
     super(props);
     this.state = {
-      password: '',
-      confirmPassword: '',
-    }
+      password: "",
+      confirmPassword: ""
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -76,7 +73,7 @@ class ResetPasswordPage extends Component<Props, State> {
   dynSetState(key: keyof State, value: string) {
     this.setState({
       [key]: value
-    } as Pick<State, keyof State>)
+    } as Pick<State, keyof State>);
   }
 
   handleChange(event) {
@@ -84,14 +81,14 @@ class ResetPasswordPage extends Component<Props, State> {
     // console.dir(event.target)
   }
 
-  handleReset = (event) => {
+  handleReset = event => {
     event.preventDefault();
     if (this.state.password !== this.state.confirmPassword) {
-      this.props.showDialog('aaa')
+      this.props.showDialog("aaa");
     }
     // this.props.login(this.state)
-    console.log(this.state)
-  }
+    console.log(this.state);
+  };
   render() {
     const { classes } = this.props;
     return (
@@ -100,15 +97,31 @@ class ResetPasswordPage extends Component<Props, State> {
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h5">
             Set your new password
-        </Typography>
+          </Typography>
           <form className={classes.form} onSubmit={this.handleReset}>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="password">New Password</InputLabel>
-              <Input name="password" type="password" id="password" autoComplete="current-password" value={this.state.password} onChange={this.handleChange} />
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
             </FormControl>
             <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
-              <Input name="confirmPassword" type="password" id="confirmPassword" autoComplete="current-password" value={this.state.confirmPassword} onChange={this.handleChange} />
+              <InputLabel htmlFor="confirmPassword">
+                Confirm Password
+              </InputLabel>
+              <Input
+                name="confirmPassword"
+                type="password"
+                id="confirmPassword"
+                autoComplete="current-password"
+                value={this.state.confirmPassword}
+                onChange={this.handleChange}
+              />
             </FormControl>
             <Button
               type="submit"
@@ -118,7 +131,7 @@ class ResetPasswordPage extends Component<Props, State> {
               className={classes.submit}
             >
               Confirm
-          </Button>
+            </Button>
           </form>
         </Paper>
       </main>
@@ -126,5 +139,7 @@ class ResetPasswordPage extends Component<Props, State> {
   }
 }
 
-
-export default connect(null, mapDispatchToProps)(withStyles(styles)(ResetPasswordPage));
+export default connect(
+  null,
+  mapDispatchToProps
+)(withStyles(styles)(ResetPasswordPage));
