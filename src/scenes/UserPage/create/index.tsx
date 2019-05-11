765a1947813c3588a270435bd5d75909a02252e3
Syoongy <mastersyong@gmail.com>
2019-05-11 20:22:00 +0800

    clean user page

--- src/scenes/UserPage/create/index.tsx
+++ src/scenes/UserPage/create/index.tsx
@@ -1,52 +1,32 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import {
   Typography,
-  Theme,
   createStyles,
   WithStyles,
-  withStyles,
-  Grid,
-  Paper,
-  TextField,
-  Divider,
-  FormControl,
-  Checkbox,
-  FormControlLabel,
-  Button,
-  MenuItem,
-  InputLabel,
-  Select
+  withStyles
 } from "@material-ui/core";
-import CustomButton from "../component/CustomButton";
-import Avatar from 'react-avatar-edit'
 import { mapDispatchToProps } from "../../../helper/dispachProps";
 import { connect } from "react-redux";
 import { SharedDispatchProps } from "../../../interface/propsInterface";
-import { history } from "../../../store"
-import FormPage from "../component/form"
+import { history } from "../../../store";
+import FormPage from "../component/form";
 
-const styles = (theme: Theme) =>
+const styles = () =>
   createStyles({
     root: {
       flexGrow: 1
-    },
+    }
   });
 
-
-export interface Props extends WithStyles<typeof styles>, SharedDispatchProps { }
-
+export interface Props extends WithStyles<typeof styles>, SharedDispatchProps {}
 
 class CreateUserPage extends Component<Props> {
-
-
-
   handleCreateUser = (e, data) => {
     // console.log(this.props.business_titleList)
     e.preventDefault();
-    this.props.createUser(data)
-    history.push('/user')
-  }
+    this.props.createUser(data);
+    history.push("/user");
+  };
 
   render() {
     const { classes } = this.props;
@@ -54,11 +34,18 @@ class CreateUserPage extends Component<Props> {
       <div className={classes.root}>
         <Typography component="h1" variant="h5">
           New User
-      </Typography>
-        <FormPage create={true} updateData='' onSubmit={(e, data) => this.handleCreateUser(e, data)} />
+        </Typography>
+        <FormPage
+          create={true}
+          updateData=""
+          onSubmit={(e, data) => this.handleCreateUser(e, data)}
+        />
       </div>
     );
   }
 }
 
-export default connect(null, mapDispatchToProps)(withStyles(styles)(CreateUserPage));
+export default connect(
+  null,
+  mapDispatchToProps
+)(withStyles(styles)(CreateUserPage));
--- src/scenes/UserPage/index.tsx
+++ src/scenes/UserPage/index.tsx
@@ -12,21 +12,19 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { render } from "react-dom";
 import CustomButton from "./component/CustomButton";
 import { RootState } from "../../reducer";
 import { mapDispatchToProps } from "../../helper/dispachProps";
 import { connect } from "react-redux";
 import { SharedDispatchProps } from "../../interface/propsInterface";
 import { User } from "../../interface/userInterface";
-import { Button, IconButton } from "@material-ui/core";
-import DeleteIcon from '@material-ui/icons/Delete';
+import { IconButton } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { history } from "../../store";
-import UpdateIcon from '@material-ui/icons/PlaylistAddCheck';
-import ResetIcon from '@material-ui/icons/BorderColor';
-import ResetPassword from './component/resetPassword';
+import UpdateIcon from "@material-ui/icons/PlaylistAddCheck";
+import ResetIcon from "@material-ui/icons/BorderColor";
+import ResetPassword from "./component/resetPassword";
 import { Company } from "../../interface/companyInterface";
-
 
 const CustomTableCell = withStyles(theme => ({
   head: {
@@ -55,67 +53,67 @@ const styles = (theme: Theme) =>
     },
     logo: {
       height: "20px"
-    },
+    }
   });
 
-export interface Props extends WithStyles<typeof styles>, SharedDispatchProps, InState { }
+export interface Props
+  extends WithStyles<typeof styles>,
+    SharedDispatchProps,
+    InState {}
 
 interface State {
-  resetPassword: boolean
+  resetPassword: boolean;
 }
 
 interface InState {
-  selectedCompany: Company,
-  userList: User[],
-  currentUserEmail: string,
+  selectedCompany: Company;
+  userList: User[];
+  currentUserEmail: string;
 }
 class CustomizedTable extends React.Component<Props, State> {
-
   state = {
-    resetPassword: false,
-  }
+    resetPassword: false
+  };
 
   componentDidMount() {
-    console.log('UserPage MOunt')
-    if (this.props.selectedCompany.company_id === '') {
+    console.log("UserPage MOunt");
+    if (this.props.selectedCompany.company_id === "") {
       let data = {
-        type: 'warning',
-        object: 'Please Select a Company first',
-        id: '1'
-      }
-      this.props.showDialog(data)
-    }
-    else this.props.getUserList()
+        type: "warning",
+        object: "Please Select a Company first",
+        id: "1"
+      };
+      this.props.showDialog(data);
+    } else this.props.getUserList();
   }
 
-  handleUpdateButtonClick = (user) => {
-    this.props.selectUser(user)
-    history.push('/user/update')
-    console.log('clicked')
-  }
+  handleUpdateButtonClick = user => {
+    this.props.selectUser(user);
+    history.push("/user/update");
+    console.log("clicked");
+  };
 
-  handleResetPassword = (user) => {
-    this.props.selectUser(user)
-    this.setState({ resetPassword: true })
-    console.log('clicked')
-  }
+  handleResetPassword = user => {
+    this.props.selectUser(user);
+    this.setState({ resetPassword: true });
+    console.log("clicked");
+  };
 
   handleClose = () => {
-    this.setState({ resetPassword: false })
-  }
+    this.setState({ resetPassword: false });
+  };
   handleDelete = (id, index) => {
-
     const payload = {
-      type: 'delete',
-      object: 'user',
-      id: id,
-    }
-    this.props.showDialog(payload)
-  }
+      type: "delete",
+      object: "user",
+      id: id
+    };
+    this.props.showDialog(payload);
+  };
 
   handleNewUser = () => {
-    history.push('/user/create')
-  }
+    history.push("/user/create");
+  };
 
   render() {
     const { classes } = this.props;
@@ -135,25 +133,48 @@ class CustomizedTable extends React.Component<Props, State> {
                 <CustomTableCell align="left">Action</CustomTableCell>
               </TableRow>
             </TableHead>
-            {this.props.userList.length > 0 && <TableBody>
-              {this.props.userList.map((row, index) => (
-                <TableRow className={classes.row} key={row.email}>
-                  <CustomTableCell component="th" scope="row"><img className={classes.logo} src={row.image} /></CustomTableCell>
-                  <CustomTableCell align="left">{row.firstname}</CustomTableCell>
-                  <CustomTableCell align="left">{row.lastname}</CustomTableCell>
-                  <CustomTableCell align="left">{row.email}</CustomTableCell>
-                  <CustomTableCell align="left">{row.status}</CustomTableCell>
-                  <CustomTableCell align="left">
-                    <IconButton onClick={() => this.handleUpdateButtonClick(row)}><UpdateIcon /></IconButton>
-                    <IconButton onClick={() => this.handleResetPassword(row)}><ResetIcon /></IconButton>
-                    {row.email !== this.props.currentUserEmail &&<IconButton onClick={() => this.handleDelete(row.email, index)}><DeleteIcon /></IconButton>}
-                  </CustomTableCell>
-                </TableRow>
-              ))}
-            </TableBody>}
+            {this.props.userList.length > 0 && (
+              <TableBody>
+                {this.props.userList.map((row, index) => (
+                  <TableRow className={classes.row} key={row.email}>
+                    <CustomTableCell component="th" scope="row">
+                      <img className={classes.logo} src={row.image} />
+                    </CustomTableCell>
+                    <CustomTableCell align="left">
+                      {row.firstname}
+                    </CustomTableCell>
+                    <CustomTableCell align="left">
+                      {row.lastname}
+                    </CustomTableCell>
+                    <CustomTableCell align="left">{row.email}</CustomTableCell>
+                    <CustomTableCell align="left">{row.status}</CustomTableCell>
+                    <CustomTableCell align="left">
+                      <IconButton
+                        onClick={() => this.handleUpdateButtonClick(row)}
+                      >
+                        <UpdateIcon />
+                      </IconButton>
+                      <IconButton onClick={() => this.handleResetPassword(row)}>
+                        <ResetIcon />
+                      </IconButton>
+                      {row.email !== this.props.currentUserEmail && (
+                        <IconButton
+                          onClick={() => this.handleDelete(row.email, index)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      )}
+                    </CustomTableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            )}
           </Table>
         </Paper>
-        <ResetPassword open={this.state.resetPassword} handleClose={this.handleClose} />
+        <ResetPassword
+          open={this.state.resetPassword}
+          handleClose={this.handleClose}
+        />
       </main>
     );
   }
@@ -167,8 +188,11 @@ function mapStateToProps(state: RootState) {
   return {
     selectedCompany: state.companyReducer.selectedCompany,
     userList: state.userReducer.userList,
-    currentUserEmail: state.authenticationReducer.email,
-  }
+    currentUserEmail: state.authenticationReducer.email
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(CustomizedTable));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(CustomizedTable));
