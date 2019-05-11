e234f2b1145beaf2e9d59f1d9a76a13df25c6bea
Syoongy <mastersyong@gmail.com>
2019-05-11 20:12:30 +0800

    clean subcompany page

--- src/scenes/SubCompanyPage/create/index.tsx
+++ src/scenes/SubCompanyPage/create/index.tsx
@@ -5,59 +5,46 @@ import {
   Theme,
   createStyles,
   WithStyles,
-  withStyles,
-  Grid,
-  Paper,
-  TextField,
-  Divider,
-  FormControl,
-  Checkbox,
-  FormControlLabel,
-  Button,
-  Select,
-  MenuItem,
-  InputLabel
+  withStyles
 } from "@material-ui/core";
-import CustomButton from "../component/CustomButton";
-import Avatar from 'react-avatar-edit'
 import { mapDispatchToProps } from "../../../helper/dispachProps";
 import { connect } from "react-redux";
 import { SharedDispatchProps } from "../../../interface/propsInterface";
 import { RootState } from "../../../reducer";
-import { Country, CountryState, Currency } from "../../../interface/countryInterface";
+import { CountryState } from "../../../interface/countryInterface";
 import { history } from "../../../store";
-import { Company, CREATECOMPANYCRED, CREATEENTITYCRED } from "../../../interface/companyInterface";
-import FormPage from "../component/form"
+import { Company } from "../../../interface/companyInterface";
+import FormPage from "../component/form";
 
-const styles = (theme: Theme) =>
+const styles = () =>
   createStyles({
     root: {
       flexGrow: 1
-    },
+    }
   });
 
-export interface Props extends WithStyles<typeof styles>, SharedDispatchProps, InState { }
+export interface Props
+  extends WithStyles<typeof styles>,
+    SharedDispatchProps,
+    InState {}
 
 interface InState {
-  paremeterList: CountryState,
-  selectedCompany: Company,
+  paremeterList: CountryState;
+  selectedCompany: Company;
 }
 
 class CreateSubCompanyPage extends Component<Props> {
-
-
   constructor(props) {
-    super(props)
-    this.handleCreateCompany = this.handleCreateCompany.bind(this)
+    super(props);
+    this.handleCreateCompany = this.handleCreateCompany.bind(this);
   }
 
+  handleCreateCompany = (e, data) => {
+    e.preventDefault();
 
-  handleCreateCompany = (e,data) => {
-    e.preventDefault()
-
-    this.props.createEntity(data)
-    history.goBack()
-  }
+    this.props.createEntity(data);
+    history.goBack();
+  };
 
   render() {
     const { classes } = this.props;
@@ -65,9 +52,13 @@ class CreateSubCompanyPage extends Component<Props> {
       <div className={classes.root}>
         <Typography component="h1" variant="h5">
           New Entity
-      </Typography>
-        <FormPage create={true} updateData='' onSubmit={(e, data) => this.handleCreateCompany(e, data)} />
-      </div >
+        </Typography>
+        <FormPage
+          create={true}
+          updateData=""
+          onSubmit={(e, data) => this.handleCreateCompany(e, data)}
+        />
+      </div>
     );
   }
 }
@@ -80,7 +71,10 @@ function mapStateToProps(state: RootState) {
   return {
     paremeterList: state.countryReducer,
     selectedCompany: state.companyReducer.selectedCompany
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(CreateSubCompanyPage));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(CreateSubCompanyPage));
