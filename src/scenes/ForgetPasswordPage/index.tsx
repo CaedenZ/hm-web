054987ff69cf5ddc4ff6467ebd2bc9f9d9bfff5d
Syoongy <mastersyong@gmail.com>
2019-05-11 20:00:28 +0800

    clean forget password page

--- src/scenes/ForgetPasswordPage/index.tsx
+++ src/scenes/ForgetPasswordPage/index.tsx
@@ -1,54 +1,53 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import { connect } from 'react-redux';
-import { mapDispatchToProps } from '../../helper/dispachProps';
-import { SharedDispatchProps } from '../../interface/propsInterface';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles, { WithStyles } from "@material-ui/core/styles/withStyles";
+import { connect } from "react-redux";
+import { mapDispatchToProps } from "../../helper/dispachProps";
+import { SharedDispatchProps } from "../../interface/propsInterface";
 
 const styles = (theme: any) => ({
   main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
       width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
   },
   paper: {
     marginTop: theme.spacing.unit * 8,
-    display: 'flex',
+    display: "flex",
     // flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
   },
   avatar: {
     margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
   },
   submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
+    marginTop: theme.spacing.unit * 3
+  }
 });
 
-export interface Props extends SharedDispatchProps, InState, WithStyles<typeof styles> { }
+export interface Props
+  extends SharedDispatchProps,
+    InState,
+    WithStyles<typeof styles> {}
 
 interface InState {
   usertoken: string;
@@ -59,7 +58,6 @@ export interface State {
 }
 
 class ForgetPasswordPage extends Component<Props, State> {
-
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -68,10 +66,9 @@ class ForgetPasswordPage extends Component<Props, State> {
   }
 
   state = {
-    email: 'zfy0120@gmail.com',
-    app_key: 'p9Eg6HN7FFXjA9WTNZ5n'
-  }
-
+    email: "zfy0120@gmail.com",
+    app_key: "p9Eg6HN7FFXjA9WTNZ5n"
+  };
 
   handleChange(event) {
     this.dynSetState(event.target.id, event.target.value);
@@ -81,13 +78,13 @@ class ForgetPasswordPage extends Component<Props, State> {
   dynSetState(key: keyof State, value: string) {
     this.setState({
       [key]: value
-    } as Pick<State, keyof State>)
+    } as Pick<State, keyof State>);
   }
-  handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(this.state)
-    this.props.forgetPassword(this.state)
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.forgetPassword(this.state);
+  };
 
   render() {
     const { classes } = this.props;
@@ -98,20 +95,22 @@ class ForgetPasswordPage extends Component<Props, State> {
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h5">
             Enter your email
-        </Typography>
+          </Typography>
           <form onSubmit={this.handleSubmit} className={classes.form}>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input id="email" name="email" autoComplete="email" autoFocus value={this.state.email} onChange={this.handleChange} />
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
             </FormControl>
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              type="submit"
-            >
+            <Button fullWidth variant="contained" color="primary" type="submit">
               Submit
-          </Button>
+            </Button>
           </form>
         </Paper>
       </main>
@@ -119,5 +118,7 @@ class ForgetPasswordPage extends Component<Props, State> {
   }
 }
 
-
-export default connect(null, mapDispatchToProps)(withStyles(styles)(ForgetPasswordPage));
+export default connect(
+  null,
+  mapDispatchToProps
+)(withStyles(styles)(ForgetPasswordPage));
