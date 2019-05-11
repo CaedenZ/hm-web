b24dc4034cdf44762be4a5ec73c361d290882aac
Syoongy <mastersyong@gmail.com>
2019-05-11 19:07:14 +0800

    clean jobgrade

--- src/scenes/CompensationSetup/JobGradePage/component/form.tsx
+++ src/scenes/CompensationSetup/JobGradePage/component/form.tsx
@@ -1,220 +1,211 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import {
-    Typography,
-    Theme,
-    createStyles,
-    WithStyles,
-    withStyles,
-    Grid,
-    Paper,
-    TextField,
-    Divider,
-    FormControl,
-    Checkbox,
-    FormControlLabel,
-    Button,
-    MenuItem,
-    InputLabel,
-    Select
+  Typography,
+  Theme,
+  createStyles,
+  WithStyles,
+  withStyles,
+  Grid,
+  Paper,
+  TextField,
+  Divider,
+  FormControl,
+  Checkbox,
+  FormControlLabel,
+  Button,
+  MenuItem,
+  InputLabel,
+  Select
 } from "@material-ui/core";
-import CustomButton from "./CustomButton";
-import Avatar from 'react-avatar-edit'
 import { mapDispatchToProps } from "../../../../helper/dispachProps";
 import { connect } from "react-redux";
 import { SharedDispatchProps } from "../../../../interface/propsInterface";
-import { history } from "../../../../store"
 import { RootState } from "../../../../reducer";
-import { Country } from "../../../../interface/countryInterface";
-import { User } from "../../../../interface/userInterface";
 import { Company } from "../../../../interface/companyInterface";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1
-        },
-        grid: {
-            margin: 20
-        },
-        textField: {
-            width: 200,
-            margin: 20,
-        },
-        // formControl: {
-        //   margin: theme.spacing.unit * 3,
-        // },
-        paper: {
-            padding: theme.spacing.unit * 2,
-            textAlign: "center",
-            color: theme.palette.text.secondary,
-            flexDirection: "column"
-        },
-        preview: {
-        },
-        divAvatar: {
-            margin: theme.spacing.unit * 3,
-            alignSelf: "baseline",
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center"
-        },
-        bigAvatar: {
-            width: "auto",
-            height: "auto"
-        },
-        profilebutton: {
-            alignContent: "center",
-            alignSelf: "center",
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center"
-        },
-        formControl: {
-            margin: theme.spacing.unit,
-            minWidth: 120,
-        },
-    });
+  createStyles({
+    textField: {
+      width: "20rem",
+      margin: "1rem"
+    }
+  });
 
 interface FormState {
-    jobgrade_name: string;
-    type: string;
-    global: boolean;
-    jobgrade_id: string;
-    country: string;
+  jobgrade_name: string;
+  type: string;
+  global: boolean;
+  jobgrade_id: string;
+  country: string;
 }
-interface Props extends InState, WithStyles<typeof styles>, SharedDispatchProps { }
+interface Props
+  extends InState,
+    WithStyles<typeof styles>,
+    SharedDispatchProps {}
 
 interface InState {
-    selectedCompany: Company;
-    create: boolean,
-    updateData: any,
-    onSubmit: any,
+  selectedCompany: Company;
+  create: boolean;
+  updateData: any;
+  onSubmit: any;
 }
 
 class FormPage extends Component<Props, FormState> {
+  constructor(props) {
+    super(props);
+  }
 
-
-    constructor(props) {
-        super(props)
+  state: FormState = {
+    jobgrade_name: "",
+    type: "",
+    global: false,
+    jobgrade_id: "",
+    country: ""
+  };
+  componentDidMount() {
+    if (!this.props.create) {
+      this.setState(this.props.updateData);
+      console.log(this.props.updateData);
     }
+  }
 
+  handleChange = (statekay: keyof FormState) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    this.setState({ [statekay]: event.target.value } as any);
+  };
 
-    state: FormState = {
-        jobgrade_name: '',
-        type: '',
-        global: false,
-        jobgrade_id: '',
-        country: '',
-    }
-    componentDidMount() {
-        if (!this.props.create) {
-            this.setState(this.props.updateData)
-            console.log(this.props.updateData)
-        }
-    }
+  handleChangeSelect = (statekay: keyof FormState) => (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    this.setState(({ [statekay]: event.target.value } as unknown) as Pick<
+      FormState,
+      keyof FormState
+    >);
+  };
 
+  handleChangeCheck = () => {
+    this.setState({ global: !this.state.global } as any);
+    this.setState({ country: "" });
+  };
 
-    handleChange = (statekay: keyof FormState) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ [statekay]: event.target.value } as any);
-    };
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper style={{ marginTop: "2rem" }}>
+        <form
+          onSubmit={e => this.props.onSubmit(e, this.state)}
+          style={{ padding: "2rem" }}
+        >
+          <Typography component="h1" variant="h6">
+            Grade Infomation
+          </Typography>
+          <Grid justify="center" container>
+            <Grid justify={"center"} container item>
+              <TextField
+                id="jobgrade_name"
+                label="jobgrade_name"
+                className={classes.textField}
+                value={this.state.jobgrade_name}
+                onChange={this.handleChange("jobgrade_name")}
+                margin="normal"
+              />
+            </Grid>
+            <Grid justify={"center"} container item>
+              <FormControl>
+                <InputLabel style={{ marginLeft: "20px" }} required>
+                  Type
+                </InputLabel>
+                <Select
+                  id="type"
+                  className={classes.textField}
+                  value={this.state.type}
+                  onChange={this.handleChangeSelect("type")}
+                  inputProps={{
+                    name: "type",
+                    id: "type-simple"
+                  }}
+                >
+                  <MenuItem value="Technical">Technical</MenuItem>
+                  <MenuItem value="Non-Technical">Non-Technical</MenuItem>
+                  <MenuItem value="Executive">Executive</MenuItem>
+                  <MenuItem value="Non-Executive">Non-Executive</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid justify={"center"} container item>
+              {this.props.selectedCompany.country.length > 0 && (
+                <FormControl>
+                  <InputLabel style={{ marginLeft: "20px" }} required>
+                    Country
+                  </InputLabel>
+                  <Select
+                    disabled={this.state.global}
+                    id="country"
+                    className={classes.textField}
+                    value={this.state.country}
+                    onChange={this.handleChangeSelect("country")}
+                    inputProps={{
+                      name: "country",
+                      id: "country-simple"
+                    }}
+                  >
+                    {this.props.selectedCompany.country.map(country => (
+                      <MenuItem
+                        key={JSON.parse(country).country_name}
+                        value={JSON.parse(country).country_name}
+                      >
+                        {JSON.parse(country).country_name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              )}
+            </Grid>
+            <Grid justify={"center"} container item>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.global}
+                    onChange={this.handleChangeCheck}
+                    color="primary"
+                  />
+                }
+                label="Global"
+              />
+            </Grid>
+          </Grid>
 
-    handleChangeSelect = (statekay: keyof FormState) => (event: React.ChangeEvent<HTMLSelectElement>) => {
-        this.setState({ [statekay]: event.target.value } as unknown as Pick<FormState, keyof FormState>);
-    };
-
-    handleChangeCheck = () => {
-        this.setState({ global: !this.state.global } as any);
-        this.setState({ country: '' })
-    };
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <Paper>
-                <form onSubmit={(e) => this.props.onSubmit(e, this.state)}>
-                    <Typography component="h1" variant="h6">Grade Infomation</Typography>
-                    <Grid justify="center" container>
-                        <Grid justify={"center"} container item>
-                            <TextField
-                                id="jobgrade_name"
-                                label="jobgrade_name"
-                                className={classes.textField}
-                                value={this.state.jobgrade_name}
-                                onChange={this.handleChange('jobgrade_name')}
-                                margin="normal"
-                            />
-                        </Grid>
-                        <Grid justify={"center"} container item>
-                            <FormControl>
-                                <InputLabel required>Type</InputLabel>
-                                <Select
-                                    id="type"
-                                    className={classes.textField}
-                                    value={this.state.type}
-                                    onChange={this.handleChangeSelect('type')}
-                                    inputProps={{
-                                        name: 'type',
-                                        id: 'type-simple',
-                                    }}>
-                                    <MenuItem value="Technical">Technical</MenuItem>
-                                    <MenuItem value="Non-Technical">Non-Technical</MenuItem>
-                                    <MenuItem value="Executive">Executive</MenuItem>
-                                    <MenuItem value="Non-Executive">Non-Executive</MenuItem>
-                                </Select></FormControl>
-                        </Grid>
-                        <Grid justify={"center"} container item>
-                            {this.props.selectedCompany.country.length > 0 && <FormControl>
-                                <InputLabel required>Country</InputLabel>
-                                <Select
-                                    disabled={this.state.global}
-                                    id="country"
-                                    className={classes.textField}
-                                    value={this.state.country}
-                                    onChange={this.handleChangeSelect('country')}
-                                    inputProps={{
-                                        name: 'country',
-                                        id: 'country-simple',
-                                    }}>
-                                    {this.props.selectedCompany.country.map((country) =>
-                                        <MenuItem key={JSON.parse(country).country_name} value={JSON.parse(country).country_name}>{JSON.parse(country).country_name}</MenuItem>
-                                    )}
-                                </Select></FormControl>}
-                        </Grid>
-                        <Grid justify={"center"} container item>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={this.state.global}
-                                        onChange={this.handleChangeCheck}
-                                        color="primary"
-                                    />
-                                }
-                                label="Global"
-                            />
-                        </Grid>
-                    </Grid>
-
-                    <Divider />
-                    <Divider />
-                    <div style={{
-                        width: '100%', flex: 1,
-                        flexDirection: 'row',
-                        justifyContent: 'center', alignItems: 'flex-end'
-                    }}>
-                        <Button variant="contained" color="primary" type="submit">Submit</Button>
-                    </div>
-                </form>
-            </Paper >
-        );
-    }
+          <Divider />
+          <Divider />
+          <div
+            style={{
+              display: "flex",
+              paddingTop: "1rem"
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              style={{ marginLeft: "auto" }}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Paper>
+    );
+  }
 }
-
 
 function mapStateToProps(state: RootState) {
-    return {
-        selectedCompany: state.companyReducer.selectedCompany,
-    }
+  return {
+    selectedCompany: state.companyReducer.selectedCompany
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(FormPage));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(FormPage));
--- src/scenes/CompensationSetup/JobGradePage/component/table.tsx
+++ src/scenes/CompensationSetup/JobGradePage/component/table.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
-    createStyles,
-    Theme,
-    withStyles,
-    WithStyles
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
 } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -12,11 +12,22 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { Button, IconButton, FormControl, InputLabel, Select, MenuItem, Grid, FormControlLabel, Checkbox, Menu } from "@material-ui/core";
-import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  Button,
+  IconButton,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+  Menu
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { history } from "../../../../store";
-import UpdateIcon from '@material-ui/icons/PlaylistAddCheck';
-import ListIcon from '@material-ui/icons/List';
+import UpdateIcon from "@material-ui/icons/PlaylistAddCheck";
+import ListIcon from "@material-ui/icons/List";
 import { SharedDispatchProps } from "../../../../interface/propsInterface";
 import { Country } from "../../../../interface/countryInterface";
 import { Company } from "../../../../interface/companyInterface";
@@ -25,136 +36,148 @@ import { RootState } from "../../../../reducer";
 import { mapDispatchToProps } from "../../../../helper/dispachProps";
 import { connect } from "react-redux";
 
-
 const CustomTableCell = withStyles(theme => ({
-    head: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white
-    },
-    body: {
-        fontSize: 14
-    }
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
 }))(TableCell);
 
 const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            width: "100%",
-            marginTop: theme.spacing.unit * 3,
-            overflowX: "auto"
-        },
-        table: {
-            minWidth: 700
-        },
-        row: {
-            "&:nth-of-type(odd)": {
-                backgroundColor: theme.palette.background.default
-            }
-        },
-        logo: {
-            height: "20px"
-        },
-    });
+  createStyles({
+    root: {
+      width: "100%",
+      marginTop: theme.spacing.unit * 3,
+      overflowX: "auto"
+    },
+    table: {
+      minWidth: 700
+    },
+    row: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.background.default
+      }
+    },
+    logo: {
+      height: "20px"
+    }
+  });
 
-export interface Props extends WithStyles<typeof styles>, SharedDispatchProps, InState { }
+export interface Props
+  extends WithStyles<typeof styles>,
+    SharedDispatchProps,
+    InState {}
 
-interface State {
-}
+interface State {}
 
 interface InState {
-    selectedCompany: Company,
-    jobgradeList: JobGrade[],
+  selectedCompany: Company;
+  jobgradeList: JobGrade[];
 }
 class CustomizedTable extends React.Component<Props, State> {
+  state = {
+    country: "",
+    global: false,
+    anchorEl: null
+  };
 
-    state = {
-        country: '',
-        global: false,
-        anchorEl: null,
-    }
+  componentDidMount() {
+    console.log("JobGradePage MOunt");
+    if (this.props.selectedCompany.company_id === "") {
+      let data = {
+        type: "warning",
+        object: "Please Select a Company first",
+        id: "1"
+      };
+      this.props.showDialog(data);
+    } else this.props.getJobGradeList();
+  }
 
-    componentDidMount() {
-        console.log('JobGradePage MOunt')
-        if (this.props.selectedCompany.company_id === '') {
-            let data = {
-                type: 'warning',
-                object: 'Please Select a Company first',
-                id: '1'
-            }
-            this.props.showDialog(data)
-        }
-        else this.props.getJobGradeList()
-    }
+  handleUpdateButtonClick = jobgrade => {
+    this.props.selectJobGrade(jobgrade);
+    history.push("/jobgrade/update");
+    console.log("clicked");
+  };
 
-    handleUpdateButtonClick = (jobgrade) => {
-        this.props.selectJobGrade(jobgrade)
-        history.push('/jobgrade/update')
-        console.log('clicked')
-    }
-
-    handleDelete = (id, index) => {
-
-        const payload = {
-            type: 'delete',
-            object: 'jobgrade',
-            id: id,
-        }
-        this.props.showDialog(payload)
-    }
-
-    handleListButtonClick = (event, row) => {
-        this.setState({ anchorEl: event.currentTarget });
-        this.props.selectJobGrade(row)
-    }
-
-    handleClose = () => {
-        this.setState({ anchorEl: null });
+  handleDelete = (id, index) => {
+    const payload = {
+      type: "delete",
+      object: "jobgrade",
+      id: id
     };
+    this.props.showDialog(payload);
+  };
 
-    handleRedirect = (path) => {
-        history.push('/jobgrade/' + path)
-    }
+  handleListButtonClick = (event, row) => {
+    this.setState({ anchorEl: event.currentTarget });
+    this.props.selectJobGrade(row);
+  };
 
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
-    render() {
-        const { classes } = this.props;
+  handleRedirect = path => {
+    history.push("/jobgrade/" + path);
+  };
 
-        return (
-            <Paper className={classes.root}>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            <CustomTableCell align="left">Grade</CustomTableCell>
-                            <CustomTableCell align="left">Type</CustomTableCell>
-                            <CustomTableCell align="left">Action</CustomTableCell>
-                        </TableRow>
-                    </TableHead>
-                    {this.props.jobgradeList.length > 0 && <TableBody>
-                        {this.props.jobgradeList.map((row, index) => (
-                            <TableRow className={classes.row} key={row.jobgrade_id}>
-                                <CustomTableCell component="th" scope="row">{row.jobgrade_name}</CustomTableCell>
-                                <CustomTableCell align="left">{row.type}</CustomTableCell>
-                                <CustomTableCell align="left">
-                                    <IconButton onClick={() => this.handleUpdateButtonClick(row)}><UpdateIcon /></IconButton>
-                                    <IconButton onClick={() => this.handleDelete(row.jobgrade_id, index)}><DeleteIcon /></IconButton>
-                                </CustomTableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>}
-                </Table>
-            </Paper>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <CustomTableCell align="left">Grade</CustomTableCell>
+              <CustomTableCell align="left">Type</CustomTableCell>
+              <CustomTableCell align="left">Action</CustomTableCell>
+            </TableRow>
+          </TableHead>
+          {this.props.jobgradeList.length > 0 && (
+            <TableBody>
+              {this.props.jobgradeList.map((row, index) => (
+                <TableRow className={classes.row} key={row.jobgrade_id}>
+                  <CustomTableCell component="th" scope="row">
+                    {row.jobgrade_name}
+                  </CustomTableCell>
+                  <CustomTableCell align="left">{row.type}</CustomTableCell>
+                  <CustomTableCell align="left">
+                    <IconButton
+                      onClick={() => this.handleUpdateButtonClick(row)}
+                    >
+                      <UpdateIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => this.handleDelete(row.jobgrade_id, index)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </CustomTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          )}
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 (CustomizedTable as React.ComponentClass<Props>).propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 } as any;
 
 function mapStateToProps(state: RootState) {
-    return {
-        selectedCompany: state.companyReducer.selectedCompany,
-    }
+  return {
+    selectedCompany: state.companyReducer.selectedCompany
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(CustomizedTable));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(CustomizedTable));
