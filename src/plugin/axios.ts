28423cb086d9691ccf8aca892b0f034ef20b04f9
Syoongy <mastersyong@gmail.com>
2019-05-16 23:15:24 +0800

    redirect if 405 statuscode is received

--- src/plugin/axios.ts
+++ src/plugin/axios.ts
@@ -1,8 +1,24 @@
-import Axios from "axios"
+import Axios from "axios";
+import { history } from "../store";
 
 const $axios = Axios.create({
-    baseURL: 'https://fwnm24zinh.execute-api.ap-southeast-1.amazonaws.com/v1',
-    /* other custom settings */
+  baseURL: "https://fwnm24zinh.execute-api.ap-southeast-1.amazonaws.com/v1"
+  /* other custom settings */
 });
+$axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    const status = response.status;
+    if (status === 405) {
+      response.data = null;
+      history.push("/");
+    }
+    return response;
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 export default $axios;
