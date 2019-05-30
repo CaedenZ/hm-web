7b257b4b6c261b171fd19a02e2e2edc4f605bda3
Syoongy <mastersyong@gmail.com>
2019-05-30 22:21:50 +0800

    add check for array

--- src/scenes/CompensationSetup/AllowancesPage/index.tsx
+++ src/scenes/CompensationSetup/AllowancesPage/index.tsx
@@ -12,12 +12,19 @@ import { mapDispatchToProps } from "../../../helper/dispachProps";
 import { connect } from "react-redux";
 import { SharedDispatchProps } from "../../../interface/propsInterface";
 import { Allowances } from "../../../interface/allowanceInterface";
-import { FormControl, InputLabel, Select, MenuItem, Grid, FormControlLabel, Checkbox } from "@material-ui/core";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  FormControlLabel,
+  Checkbox
+} from "@material-ui/core";
 import { history } from "../../../store";
 import { Company } from "../../../interface/companyInterface";
 import { Country } from "../../../interface/countryInterface";
-import CustomizedTable from './component/table'
-
+import CustomizedTable from "./component/table";
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -36,96 +43,103 @@ const styles = (theme: Theme) =>
     },
     logo: {
       height: "20px"
-    },
+    }
   });
 
-export interface Props extends WithStyles<typeof styles>, SharedDispatchProps, InState { }
+export interface Props
+  extends WithStyles<typeof styles>,
+    SharedDispatchProps,
+    InState {}
 
 interface State {
-  country: string,
-  global: boolean,
+  country: string;
+  global: boolean;
 }
 
 interface InState {
-  countryList: Country[],
-  selectedCompany: Company,
-  allowancesList: Allowances[],
+  countryList: Country[];
+  selectedCompany: Company;
+  allowancesList: Allowances[];
 }
 class AllowancesPage extends React.Component<Props, State> {
-
   state = {
-    country: '',
-    global: true,
-  }
+    country: "",
+    global: true
+  };
 
   componentDidMount() {
-    console.log('AllowancesPage MOunt')
-    if (this.props.selectedCompany.company_id === '') {
+    console.log("AllowancesPage MOunt");
+    if (this.props.selectedCompany.company_id === "") {
       let data = {
-        type: 'warning',
-        object: 'Please Select a Company first',
-        id: '1'
-      }
-      this.props.showDialog(data)
-    }
-    else this.props.getAllowancesList()
+        type: "warning",
+        object: "Please Select a Company first",
+        id: "1"
+      };
+      this.props.showDialog(data);
+    } else this.props.getAllowancesList();
   }
 
-  handleUpdateButtonClick = (allowances) => {
-    this.props.selectAllowances(allowances)
-    history.push('/allowances/update')
-    console.log('clicked')
-  }
+  handleUpdateButtonClick = allowances => {
+    this.props.selectAllowances(allowances);
+    history.push("/allowances/update");
+    console.log("clicked");
+  };
 
-  handleDelete = (id) => {
-
+  handleDelete = id => {
     const payload = {
-      type: 'delete',
-      object: 'allowances',
-      id: id,
-    }
-    this.props.showDialog(payload)
-  }
+      type: "delete",
+      object: "allowances",
+      id: id
+    };
+    this.props.showDialog(payload);
+  };
 
   handleNewGrade = () => {
-    history.push('/allowances/create')
-  }
+    history.push("/allowances/create");
+  };
 
   handleChange = () => {
     this.setState({ global: !this.state.global } as any);
-    this.setState({ country: '' } as any);
-    console.log(this.getdata())
+    this.setState({ country: "" } as any);
+    console.log(this.getdata());
   };
 
-  handleChangeSelect = (statekay: keyof State) => (event: React.ChangeEvent<HTMLSelectElement>) => {
+  handleChangeSelect = (statekay: keyof State) => (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     this.setState({ [statekay]: event.target.value } as any);
   };
 
   getdata = () => {
-    if (this.state.global) {
-      return this.props.allowancesList.filter(e => {
-        return e.jobgrade_global === 1
-      })
+    if (this.props.allowancesList instanceof Array) {
+      if (this.state.global) {
+        return this.props.allowancesList.filter(e => {
+          return e.jobgrade_global === 1;
+        });
+      } else if (this.state.country !== "") {
+        return this.props.allowancesList.filter(e => {
+          return e.country === this.state.country;
+        });
+      } else {
+        return this.props.allowancesList;
+      }
+    } else {
+      return [];
     }
-    else if (this.state.country !== '') {
-      return this.props.allowancesList.filter(e => {
-        return e.country === this.state.country
-      })
-    }
-    else {
-      return this.props.allowancesList
-    }
-  }
+  };
 
   render() {
-    let data = this.getdata()
+    let data = this.getdata();
 
     return (
       <main>
-        <CustomButton onClick={this.handleNewGrade}>Create New Allowances</CustomButton>
+        <CustomButton onClick={this.handleNewGrade}>
+          Create New Allowances
+        </CustomButton>
         <Grid container>
           <Grid item xs={3}>
-            <FormControlLabel style={{ height: '100%', width: '100%' }}
+            <FormControlLabel
+              style={{ height: "100%", width: "100%" }}
               control={
                 <Checkbox
                   checked={this.state.global}
@@ -137,25 +151,27 @@ class AllowancesPage extends React.Component<Props, State> {
             />
           </Grid>
           <Grid item xs={3}>
-            <FormControl style={{ width: '100%' }} disabled={this.state.global}>
+            <FormControl style={{ width: "100%" }} disabled={this.state.global}>
               <InputLabel htmlFor="country">Country</InputLabel>
               <Select
                 value={this.state.country}
-                onChange={this.handleChangeSelect('country')}
+                onChange={this.handleChangeSelect("country")}
                 inputProps={{
-                  name: 'country',
-                  id: 'country-simple',
+                  name: "country",
+                  id: "country-simple"
                 }}
               >
-                {this.props.selectedCompany.country.map((country) =>
-                  <MenuItem key={(country)} value={(country)}>{(country)}</MenuItem>
-                )}
+                {this.props.selectedCompany.country.map(country => (
+                  <MenuItem key={country} value={country}>
+                    {country}
+                  </MenuItem>
+                ))}
               </Select>
             </FormControl>
           </Grid>
         </Grid>
         <CustomizedTable allowancesList={data} />
-      </main >
+      </main>
     );
   }
 }
@@ -168,8 +184,11 @@ function mapStateToProps(state: RootState) {
   return {
     countryList: state.countryReducer.countryList,
     selectedCompany: state.companyReducer.selectedCompany,
-    allowancesList: state.allowancesReducer.allowancesList,
-  }
+    allowancesList: state.allowancesReducer.allowancesList
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(AllowancesPage));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(AllowancesPage));
